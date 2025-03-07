00ca1b75a5e68a21e5e6ecc55ad50f459fc0b547
sivannikov <pawga777@gmail.com>
2025-03-07 17:21:48 +0300

    initial commit
    initial commit
    
    initial commit
    
    initial commit

--- src/main/kotlin/com/pawga/service/SimpleHandler.kt
+++ src/main/kotlin/com/pawga/service/SimpleHandler.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.pawga
+package com.pawga.service
 
 import info.novatec.micronaut.camunda.external.client.feature.ExternalTaskSubscription
 import jakarta.inject.Singleton
